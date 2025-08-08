9df70ad5182b251ee70b669107867b3917eb0c60
rashidarashida <rashidaakther774@gmail.com>
2025-08-08 11:40:26 -0700

    add something

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const nameList = document.getElementById("nameList");
 const total = document.getElementById("total");
 const discountTotal = document.getElementById("discountTotal");
+const purchaseBtn = document.getElementById("purchaseBtn");
+const applyBtn = document.getElementById("applyBtn");
+const inputCoupon = document.getElementById("inputCoupon");
+const discountContainer = document.getElementById("discount");
+applyBtn.disabled = true;
+purchaseBtn.disabled = true;
 let totalPrice = 0;
 const handClick = (data) => {
   const name = data.childNodes[5].innerText;
@@ -13,4 +19,36 @@ const handClick = (data) => {
   const li = document.createElement("li");
   li.innerText = name;
   nameList.appendChild(li);
+  if(totalPrice > 0){
+    purchaseBtn.disabled = false;
+  }
+  if(totalPrice >= 200){
+    applyBtn.disabled = false;
+  }
 };
+
+
+applyBtn.addEventListener("click" , () => {
+  if(inputCoupon.value === ""){
+    alert("Input is required");
+    return;
+  }
+
+if(inputCoupon.value === "SELL200"){
+  const discountRate = 0.2;
+const discount = totalPrice * discountRate;
+const totalPriceWithDiscount = totalPrice - discount;
+discountContainer.innerText = discount.toFixed(2);
+discountTotal.innerText = totalPriceWithDiscount;
+}else{
+  alert("Coupon does not match")
+}
+});
+
+purchaseBtn.addEventListener("click", () => {
+  totalPrice = 0;
+  total.innerText = "00";
+  discountTotal.innerText = "00";
+  discountContainer.innerText = "00"
+  nameList.innerHTML = ""
+});
